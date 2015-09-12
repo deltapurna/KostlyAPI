a2b47d271b3ba0ee41c05d5f65771e0faaa86783
Delta Purna Widyangga <deltapurna@gmail.com>
2015-09-12 13:30:10 +0700

    add create place endpoint

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,4 +3,18 @@ class PlacesController < ApplicationController
     places = Place.all
     render json: places, status: 200
   end
+
+  def create
+    place = Place.new(place_params)
+    if place.save
+      render json: place, status: 201
+    else
+      render json: place.errors, status: 422
+    end
+  end
+
+  private
+    def place_params
+      params.require(:place).permit(:name)
+    end
 end
